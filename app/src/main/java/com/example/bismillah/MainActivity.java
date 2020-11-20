374bcdcadb4fdf08aec75c5ad1f05c745471493a
fadelmi12 <fadelirsyad04@gmail.com>
2020-11-20 10:27:53 +0700

    helloo

--- app/src/main/java/com/example/bismillah/MainActivity.java
+++ app/src/main/java/com/example/bismillah/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.bismillah;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -11,11 +15,13 @@ import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
     FrameLayout frameLayout;
     LinearLayout layout_tab;
-
+    TabLayout tab;
+    ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +29,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         layout_tab=(LinearLayout)findViewById(R.id.layout_tab);
         frameLayout=(FrameLayout)findViewById(R.id.frame_layout);
+        tab=(TabLayout)findViewById(R.id.tab);
+        Tab_adapter tab_adapter = new Tab_adapter(getSupportFragmentManager(), tab.getTabCount());
+        viewPager=(ViewPager)findViewById(R.id.viewpager);
+        viewPager.setAdapter(tab_adapter);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tab));
+        tab.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
 
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
         BottomNavigationView navigationView = findViewById(R.id.btn_nav);
 
         navigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -68,5 +94,32 @@ public class MainActivity extends AppCompatActivity {
         });
 
         navigationView.setSelectedItemId(R.id.home);
+    }
+}
+class Tab_adapter extends FragmentStatePagerAdapter{
+
+    int jumlahTab;
+    public Tab_adapter(@NonNull FragmentManager fm, int jmlTab) {
+        super(fm);
+        this.jumlahTab=jmlTab;
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+                tab1 Tab1=new tab1();
+                return Tab1;
+            case 1:
+                tab2 Tab2=new tab2();
+                return Tab2;
+        }
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return jumlahTab;
     }
 }
