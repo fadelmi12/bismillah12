ddfefed684b8d7c9bdd1b6ee9212fe165f3c9718
5h1ro <rajamodus7073@gmail.com>
2020-11-24 19:42:12 +0700

    benakno scrollview activity ubah data diri

---
+++ app/src/main/java/com/example/bismillah/ActivityUbahDataDiri.java
@@ -0,0 +1,14 @@
+package com.example.bismillah;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityUbahDataDiri extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ubah_data_diri);
+    }
+}
