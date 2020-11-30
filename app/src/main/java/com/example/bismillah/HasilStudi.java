b992398e414a8a6ad14ea364bc63f73ecf704ad8
fadelmi12 <fadelirsyad04@gmail.com>
2020-12-01 02:26:45 +0700

    layout hasil studi

--- app/src/main/java/com/example/bismillah/HasilStudi.java
+++ app/src/main/java/com/example/bismillah/HasilStudi.java
@@ -1,69 +1,76 @@
 package com.example.bismillah;
 
-import androidx.annotation.NonNull;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toolbar;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentStatePagerAdapter;
-import androidx.viewpager.widget.ViewPager;
-
-import android.os.Bundle;
-import android.widget.LinearLayout;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.tabs.TabLayout;
 
 public class HasilStudi extends AppCompatActivity {
-
-    TabLayout tab1;
-    LinearLayout tabLayout1;
-    ViewPager viewpager2;
+    Button btnsmt1, btnsmt2, btnsmt3, btnsmt4, btnsmt5, btnsmt6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hasil_studi);
 
-        tabLayout1 = (LinearLayout)findViewById(R.id.tablayout1);
-        tab1=(TabLayout)findViewById(R.id.tab1);
-        tab_adapter tab_Adapter = new tab_adapter(getSupportFragmentManager(), tab1.getTabCount());
-    }
-
-    class tab_adapter extends FragmentStatePagerAdapter {
-        int jumlahTab;
-        public tab_adapter(@NonNull FragmentManager fm, int jmlTab) {
-            super(fm);
-            this.jumlahTab=jmlTab;
-        }
-        @NonNull
-        @Override
-        public Fragment getItem(int position) {
-            switch (position){
-                case 0:
-                    smt1 Smt1 =new smt1();
-                    return Smt1;
-                case 1:
-                    smt2 Smt2 =new smt2();
-                    return Smt2;
-                case 2:
-                    smt3 Smt3 =new smt3();
-                    return Smt3;
-                case 3:
-                    smt4 Smt4 =new smt4();
-                    return Smt4;
-                case 4:
-                    smt5 Smt5 =new smt5();
-                    return Smt5;
-                case 5:
-                    smt6 Smt6 =new smt6();
-                    return Smt6;
+        btnsmt1 = findViewById(R.id.smt1);
+        btnsmt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HasilStudi.this, semester1.class);
+                startActivity(intent);
             }
-            return null;
-        }
+        });
+        btnsmt2 = findViewById(R.id.smt2);
+        btnsmt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HasilStudi.this, semester2.class);
+                startActivity(intent);
+            }
+        });
+        btnsmt3 = findViewById(R.id.smt3);
+        btnsmt3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HasilStudi.this, semester3.class);
+                startActivity(intent);
+            }
+        });
+        btnsmt4 = findViewById(R.id.smt4);
+        btnsmt4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HasilStudi.this, semester4.class);
+                startActivity(intent);
+            }
+        });
+        btnsmt5 = findViewById(R.id.smt5);
+        btnsmt5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HasilStudi.this, semester5.class);
+                startActivity(intent);
+            }
+        });
+        btnsmt6 = findViewById(R.id.smt6);
+        btnsmt6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HasilStudi.this, semester6.class);
+                startActivity(intent);
+            }
+        });
 
-        @Override
-        public int getCount() {
-            return jumlahTab;
-        }
     }
 
 }
